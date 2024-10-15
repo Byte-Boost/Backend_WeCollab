390a50e9391d8a3c21e9d89321827ca870f45172
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-10-15 16:23:47 -0300

    [docs](swagger): roles route done

--- routes/role.routes.js
+++ routes/role.routes.js
@@ -3,3 +3,39 @@ const router = require('express').Router();
 
 router.get('/', controller.getRoles);
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Roles
+ *   description: Role management
+ */
+
+/**
+ * @swagger
+ * /roles:
+ *   get:
+ *     tags: [Roles]
+ *     summary: Get all roles
+ *     responses:
+ *       200:
+ *         description: Got all roles successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 roles:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: "Assistente Administrativo"
+ *       400:
+ *         description: Error occurred while getting roles
+ */
