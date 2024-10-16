a127e1769b5a3ba174b8f1e198d363ab71bb92dc
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-10-15 22:17:37 -0300

    [docs](swagger): user route done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,3 +6,198 @@ router.get('/', controller.getUsers);
 router.get('/cpf/:cpf', adminMiddleware, controller.getUsersByCPF);
 router.get('/:id', adminMiddleware, controller.getUsersById);
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management
+ */
+
+/**
+ * @swagger
+ * /users/:
+ *   get:
+ *     tags: [Users]
+ *     summary: Get all users
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Got all users successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: "John Doe Smith"
+ *                       cpf:
+ *                         type: string
+ *                         example: "12423534645"
+ *                       area:
+ *                         type: string
+ *                         example: "Administrativa"
+ *                       roleId:
+ *                         type: number
+ *                         example: 1
+ *                       admin:
+ *                         type: boolean
+ *                         example: false
+ *                       createdAt:
+ *                         type: string
+ *                         format: date-time
+ *                         example: "2024-10-15T19:01:07.000Z"
+ *                       updatedAt:
+ *                         type: string
+ *                         format: date-time
+ *                         example: "2024-12-15T19:01:07.000Z"
+ *                       Role:
+ *                         type: object
+ *                         properties: 
+ *                           name:
+ *                              type: string
+ *                              example: "Assistente Administrativo"
+ *       400:
+ *         description: Error occurred while fetching users
+ *       401:
+ *         description: Unauthorized - token missing or invalid
+ */
+
+/**
+ * @swagger
+ * /users/cpf/{cpf}:
+ *   get:
+ *     tags: [Users]
+ *     summary: Get user based on CPF
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: cpf
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The user CPF
+ *     responses:
+ *       200:
+ *         description: Got user by CPF successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: "John Doe Smith"
+ *                       cpf:
+ *                         type: string
+ *                         example: "12423534645"
+ *                       area:
+ *                         type: string
+ *                         example: "Administrativa"
+ *                       roleId:
+ *                         type: number
+ *                         example: 1
+ *                       admin:
+ *                         type: boolean
+ *                         example: false
+ *                       createdAt:
+ *                         type: string
+ *                         format: date-time
+ *                         example: "2024-10-15T19:01:07.000Z"
+ *                       updatedAt:
+ *                         type: string
+ *                         format: date-time
+ *                         example: "2024-12-15T19:01:07.000Z"
+ *                       Role:
+ *                         type: object
+ *                         properties: 
+ *                           name:
+ *                             type: string
+ *                             example: "Assistente Administrativo"
+ *       401:
+ *         description: Unauthorized - token missing or invalid
+ */
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     tags: [Users]
+ *     summary: Get user based on ID
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: Got user by ID successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                       name:
+ *                         type: string
+ *                         example: "John Doe Smith"
+ *                       cpf:
+ *                         type: string
+ *                         example: "12423534645"
+ *                       area:
+ *                         type: string
+ *                         example: "Administrativa"
+ *                       roleId:
+ *                         type: number
+ *                         example: 1
+ *                       admin:
+ *                         type: boolean
+ *                         example: false
+ *                       createdAt:
+ *                         type: string
+ *                         format: date-time
+ *                         example: "2024-10-15T19:01:07.000Z"
+ *                       updatedAt:
+ *                         type: string
+ *                         format: date-time
+ *                         example: "2024-12-15T19:01:07.000Z"
+ *                       Role:
+ *                         type: object
+ *                         properties: 
+ *                           name:
+ *                              type: string
+ *                              example: "Assistente Administrativo"
+ *       400:
+ *         description: Error occurred while editing account
+ *       401:
+ *         description: Unauthorized - token missing or invalid
+ */
